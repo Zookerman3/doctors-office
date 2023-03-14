2c96664e2822ba64066f5189f900d53d0dee81b1
Stephen <szook7@gmail.com>
2023-03-14 15:54:42 -0700

    application will work as intended

--- DoctorsOffice/Controllers/DoctorsController.cs
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using DoctorsOffice.Models;
@@ -6,73 +7,91 @@ using System.Linq;
 
 namespace DoctorsOffice.Controllers
 {
-  public class DoctorsController : Controller
-  {
-    private readonly DoctorsOfficeContext _db;
-
-    public DoctorsController(DoctorsOfficeContext db)
+    public class DoctorsController : Controller
     {
-      _db = db;
-    }
+        private readonly DoctorsOfficeContext _db;
 
-    public ActionResult Index()
-    {
-      List<Doctor> model = _db.Doctors.ToList();
-      return View(model);
-    }
+        public DoctorsController(DoctorsOfficeContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Create()
-    {
-      ViewBag.Specialties = _db.Specialties.ToList();
-      return View();
-    }
+        public ActionResult Index()
+        {
+            List<Doctor> model = _db.Doctors.ToList();
+            return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Doctor doctor)
-    {
-      _db.Doctors.Add(doctor);
-      
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        public ActionResult Create()
+        {
+            ViewBag.Specialties = _db.Specialties.ToList();
+            return View();
+        }
 
-    public ActionResult Details(int id)
-    {
-      Doctor thisDoctor = _db.Doctors
-                          .Include(doctor => doctor.Patients)
-                          .Include(doctor => doctor.JoinEntities)
-                          .ThenInclude(join => join.Specialty)
-                          .FirstOrDefault(doctor => doctor.DoctorId == id);
-      return View(thisDoctor);
-    }
+        [HttpPost]
+        public ActionResult Create(Doctor doctor)
+        {
+            _db.Doctors.Add(doctor);
 
-    public ActionResult Edit(int id)
-    {
-      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
-      return View(thisDoctor);
-    }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    [HttpPost]
-    public ActionResult Edit(Doctor doctor)
-    {
-      _db.Doctors.Update(doctor);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        public ActionResult Details(int id)
+        {
+            Doctor thisDoctor = _db.Doctors
+                                .Include(doctor => doctor.Patients)
+                                .Include(doctor => doctor.JoinEntities)
+                                .ThenInclude(join => join.Specialty)
+                                .FirstOrDefault(doctor => doctor.DoctorId == id);
+            return View(thisDoctor);
+        }
 
-    public ActionResult Delete(int id)
-    {
-      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
-      return View(thisDoctor);
-    }
+        public ActionResult Edit(Doctor doctor, int id)
+        {
+            Doctor thisDoctor = _db.Doctors
+                            .Include(doctor => doctor.Patients)
+                            .Include(doctor => doctor.JoinEntities)
+                            .ThenInclude(join => join.Specialty)
+                            .FirstOrDefault(doctor => doctor.DoctorId == id);
+            ViewBag.SpecialtyId = new SelectList(_db.Specialties, "SpecialtyId", "Name");
+            return View(thisDoctor);
+        }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
-      _db.Doctors.Remove(thisDoctor);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        [HttpPost]
+        public ActionResult Edit(Doctor doctor)
+        {
+            _db.Doctors.Update(doctor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost, ActionName("UpdateDoctorSpec")]
+        public ActionResult UpdateDoctorSpec(int DoctorId, int SpecialtyId)
+        {
+            #nullable enable
+            DoctorSpecialty? joinEntity = _db.DoctorSpecialties.FirstOrDefault(join => (join.DoctorId == DoctorId && join.SpecialtyId == SpecialtyId));
+            #nullable disable
+            if (joinEntity == null && DoctorId != 0)
+            {
+                _db.DoctorSpecialties.Add(new DoctorSpecialty() { DoctorId = DoctorId, SpecialtyId = SpecialtyId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new { id = DoctorId });
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+            return View(thisDoctor);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+            _db.Doctors.Remove(thisDoctor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
